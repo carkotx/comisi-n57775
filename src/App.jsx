7bc0ad998bf2927958ec6fe4c41ddb1bd596eef2
carkotx <carkotx@gmail.com>
2024-05-29 10:13:20 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+/* App es un componente, es una funcion que va 
+a contener muchos elementos
+componentes:
+
+-son funciones
+- van en mayusculas (por el complilador
+- si o si teienen que tener retorno (return)
+ -solo se puede retornar un solo elemento, una 
+ sola etiqueta */
+
+ //https://github.com/comisiones-coderhouse/comision_57775
+
+function App (){
+  
+  return <h1 id="titulo" className="titulo">Hola mundo!</h1>//<Header/>
+}
+//App()
+//<App/>
+export default App;
