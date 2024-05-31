945dd7611b8e94dc9d3559f642356e3f8ea769e3
carkotx <carkotx@gmail.com>
2024-05-31 18:46:27 -0300

    comp1 puch

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Header from "./Header.jsx"
+
 /* App es un componente, es una funcion que va 
 a contener muchos elementos
 componentes:
@@ -8,12 +10,46 @@ componentes:
  -solo se puede retornar un solo elemento, una 
  sola etiqueta */
 
+ /*tipos de componentes:
+ - Estructurales: son lo que representan una plantilla 
+ general o un bloque generico del sitio/aplicacion 
+
+ - Funcionales: Son los que representan una funcionalidad
+ especifica o un bloque de contenido especifico
+
+
+
+ */
+
  //https://github.com/comisiones-coderhouse/comision_57775
 
 function App (){
+
+  //const titulo = document.createElement("h1")
+  //titulo.textContent = "Hola Mundo desde un p"
+  //titulo.id = "titulo"
+  //titulo.className = "titulo"
+  //return <h1 id="titulo" className="titulo">Hola Mundo desde un p</h1>
+  //Header()
+  //<Header/>
   
-  return <h1 id="titulo" className="titulo">Hola mundo!</h1>//<Header/>
+  //return <h1 id="titulo" className="titulo">Hola mundo!</h1>//<Header/>}
+
+return (
+ <>
+   <Header/>
+   <main>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Pariatur blanditiis praesentium quas, similique
+      voluptate dolorem nesciunt totam earum libero a hic
+      ad ratione illo. Illum accusantium iste placeat! 
+      Suscipit, quaerat.</p>
+   </main>
+   <footer>
+    <p>Copyright &copy; - Carlos Rangel </p>
+   </footer>
+ </>)
 }
-//App()
-//<App/>
+//App(), return y etiquetas <> es un componente
 export default App;
